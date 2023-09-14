fdc0fbe932b3eadbd31d7ba264a8446c29eb0abe
Abbira khan <abbirakhan54@gmail.com>
2023-09-14 00:04:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# advanced_calculator_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'pages/calculator.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'First Method',
+          
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+            ),
+            home: child,
+          );
+        },
+        child: const CalculatorApp());
+  }
+}
